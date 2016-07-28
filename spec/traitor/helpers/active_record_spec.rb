15217c8f4826b20ac65954113b42bf5c4579f269
Zach Lome <zach.lome@avant.com>
2016-07-28 12:59:09 -0500

    Fix ActiveRecord tests by binding to arel 6/activerecord 4.

--- spec/traitor/helpers/active_record_spec.rb
+++ spec/traitor/helpers/active_record_spec.rb
@@ -2,17 +2,42 @@ require 'traitor/helpers/active_record'
 require_relative 'active_record_helper' # create TestARClass
 
 describe 'Active Record Extensions' do
-  it 'stubs quick create' do
+  it 'stubs create without callbacks' do
     expect(TestActiveRecordClass.new).to respond_to :create_without_callbacks
   end
 
   describe '#create_without_callbacks', traitor_create_method: :create_without_callbacks do
-    it 'creates via insert' do
-      sql = 'INSERT INTO "test_active_record_classes" ("param") VALUES (\'foo\')'
-      allow(ActiveRecord::Base.connection).to receive(:execute).and_call_original
-      expect(ActiveRecord::Base.connection).to receive(:execute).with(sql).and_call_original
-      object = Traitor.create(:test_active_record_class, param: :foo)
-      expect(object.id).to eq 1
+    describe 'With SQLite' do
+      it 'creates via insert' do
+        sql = 'INSERT INTO "test_active_record_classes" ("param") VALUES (\'foo\')'
+        allow(ActiveRecord::Base.connection).to receive(:execute).and_call_original
+        expect(ActiveRecord::Base.connection).to receive(:execute).with(sql).and_call_original
+        object = Traitor.create(:test_active_record_class, param: :foo)
+        expect(object.id).to eq 1
+      end
+    end
+
+    describe 'With PGSQL' do
+      let(:insert_sql) { 'INSERT INTO "test_active_record_classes" ("param") VALUES (\'foo\')' }
+      let(:stubbed_connection) {
+        double(
+          execute: [{'id' => 1}],
+          class: double(name: 'ActiveRecord::ConnectionAdapters::PostgreSQLAdapter'),
+          # from arel
+          visitor: double(accept: double(value: insert_sql)),
+          schema_cache: ActiveRecord::Base.connection.send(:schema_cache),
+        )
+      }
+
+      before do
+        allow(ActiveRecord::Base).to receive(:connection).and_return(stubbed_connection)
+      end
+
+      it 'creates via insert' do
+        expect(stubbed_connection).to receive(:execute).with(insert_sql + ' RETURNING id')
+        object = Traitor.create(:test_active_record_class, param: :foo)
+        expect(object.id).to eq 1
+      end
     end
   end
 end
--- traitr.gemspec
+++ traitr.gemspec
@@ -17,5 +17,5 @@ Gem::Specification.new do |s|
   s.add_development_dependency 'timecop'
   s.add_development_dependency 'pry'
   s.add_development_dependency 'sqlite3'
-  s.add_development_dependency 'activerecord', '>= 4.0.0'
+  s.add_development_dependency 'activerecord', '~> 4'
 end
